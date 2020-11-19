e5c5fd2d969b327ce70adfb9e8d373d5ba643480
Xusheng Fred Liu <liux6@rose-hulman.edu>
2020-11-19 14:52:09 -0500

    Finished part 09

--- Part 09 Dart Cupertino and Widgets/bitcoin-ticker-flutter-master/lib/coin_data.dart
+++ Part 09 Dart Cupertino and Widgets/bitcoin-ticker-flutter-master/lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +31,23 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  Future getCoinData(String selectedCurrency) async {
+    Map<String, String> cryptoPrices = {};
+    for (String crypto in cryptoList) {
+      String requestURL =
+          'https://apiv2.bitcoinaverage.com/indices/global/ticker/$crypto$selectedCurrency';
+      http.Response response = await http.get(requestURL);
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        double price = decodedData['last'];
+        cryptoPrices[crypto] = price.toStringAsFixed(0);
+      } else {
+        print(response.statusCode);
+        throw 'Problem with the get request';
+      }
+    }
+    print(cryptoPrices);
+    return cryptoPrices;
+  }
+}
